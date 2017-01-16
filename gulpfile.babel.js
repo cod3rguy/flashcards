cdf310be59f789aaa5aed8e7d61a48fbd159de9b
Sean Mishra <santakmishra@outlook.com>
2017-01-16 17:05:57 -0600

    Updated build system

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,10 +2,20 @@ import gulp from 'gulp';
 import plumber from 'gulp-plumber';
 import less from 'gulp-less';
 import browserify from 'browserify';
+import source from 'vinyl-source-stream';
+import buffer from 'vinyl-buffer';
+import uglify from 'gulp-uglify';
+
+let errorHandler = function(msgSource) {
+    return function({message, plugin = msgSource}){
+        console.error( `\n${plugin}: ${message}\n`);
+        this.emit('end');
+    };
+};
 
 gulp.task('makeStyle',() => {
     return gulp.src('./styles/src/style.less')
-    .pipe(plumber())
+    .pipe(plumber(errorHandler('makeStyle')))
     .pipe(less({
         compress: true
     }))
@@ -13,10 +23,20 @@ gulp.task('makeStyle',() => {
 });
 
 gulp.task('makeScript',() => {
-    return gulp.src('./scripts/src/app.js')
-    .pipe(plumber())
-    .pipe(browserify()).transform("babelify", {presets: ["es2015", "react"]}).bundle()
+    return browserify('./scripts/src/app.jsx')
+    .transform("babelify")
+    .bundle()
+    .on('error', errorHandler('browserify'))
+    .pipe(source('app.js'))
+    .pipe(plumber(errorHandler('makeScript')))
+    .pipe(buffer())
+    .pipe(uglify())
     .pipe(gulp.dest('./scripts/'));
 });
 
-gulp.task('default', ['makeStyle','makeScript']);
+gulp.task('watch', ['makeStyle','makeScript'], () => {
+    gulp.watch(['./styles/src/*','./styles/src/includes/*'], ['makeStyle']);
+    gulp.watch(['./scripts/src/*','./scripts/src/includes/*'], ['makeScript']);
+});
+
+gulp.task('default', ['watch']);
--- scripts/src/app.js
+++
@@ -1,7 +0,0 @@
-import React from '../../node_modules/react/dist/react';
-import ReactDOM from '../../node_modules/react/dist/react-dom';
-
-ReactDOM.render(
-  <h1>Hello, world!</h1>,
-  document.getElementById('root')
-);
