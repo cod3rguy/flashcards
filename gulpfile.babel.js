07f7cfb6415f3ac459068ec7985642065055079c
Sean Mishra <santakmishra@outlook.com>
2017-01-15 23:32:29 -0600

    Updated config and project structure

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,24 +1,22 @@
 import gulp from 'gulp';
 import plumber from 'gulp-plumber';
 import less from 'gulp-less';
+import browserify from 'browserify';
 
 gulp.task('makeStyle',() => {
-    return gulp.src('./styles/less/style.less')
+    return gulp.src('./styles/src/style.less')
     .pipe(plumber())
     .pipe(less({
         compress: true
     }))
-    .pipe(gulp.dest('./styles/css/'));
+    .pipe(gulp.dest('./styles/'));
 });
 
 gulp.task('makeScript',() => {
-    return gulp.src('./less/style.less')
+    return gulp.src('./scripts/src/app.js')
     .pipe(plumber())
-    .pipe(less({
-        compress: true
-    }))
-    .pipe(gulp.dest('./css/'));
+    .pipe(browserify()).transform("babelify", {presets: ["es2015", "react"]}).bundle()
+    .pipe(gulp.dest('./scripts/'));
 });
 
-
-gulp.task('default', ['makeStyle']);
+gulp.task('default', ['makeStyle','makeScript']);
