5ab21183e29f2982779db9cbdbadcc1b282dbae2
Sean Mishra <santakmishra@outlook.com>
2017-01-18 21:41:43 -0600

    Updated routes

--- scripts/src/app.jsx
+++ scripts/src/app.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Router, Route, Redirect, hashHistory} from 'react-router';
 import Layout from './includes/layouts/default.jsx';
+import Home from './includes/pages/home.jsx';
 
 ReactDOM.render(
   <Router history={hashHistory}>
+    <Redirect from="/" to="/home" />
     <Route path="/" component={Layout}>
-      Test
+      <Route path="home" component={Home} />
     </Route>
   </Router>
   ,document.getElementById('root')
